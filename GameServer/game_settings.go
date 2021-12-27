06698c37df3ee5fd577bee411812a11ff50aa29b
Harry Sadler <harrysadlermusic@gmail.com>
2021-12-27 11:07:28 +0100

    update game settings, clear placeholder UI text

--- GameServer/game_settings.go
+++ GameServer/game_settings.go
@@ -1,7 +1,7 @@
 package main
 
-const MAP_HEIGHT int = 500
-const MAP_WIDTH int = 500
+const MAP_HEIGHT int = 200
+const MAP_WIDTH int = 200
 
 const PLAYER_RESPAWN_SECONDS = 6
 const FOOD_COUNT int = MAP_HEIGHT
